6f694cf70a4cf90bbf6784b4ef7579003119892f
choudhurynirjhar <choudhurynirjhar@gmail.com>
2012-05-06 11:15:10 -0400

    implement class resolver

---
+++ PlainIoC/PlainIoCConsole/DataProviderManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlainIoCConsole
+{
+    public class DataProviderManager
+    {
+        private IDateProvider provider;
+
+        public DataProviderManager(IDateProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public bool IsDataAvailable()
+        {
+            return provider.ProvideData() != null;
+        }
+    }
+}
---
+++ PlainIoC/PlainIoCConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlainIoC;
+using System.Reflection;
+
+namespace PlainIoCConsole
+{
+    class Program
+    {
+        private static PIoCContainer container;
+
+        static void Main(string[] args)
+        {
+            ConfigureIoC();
+            TestDataProviderManager();
+        }
+
+        static void ConfigureIoC()
+        {
+            container = new PIoCContainer();
+            container.RecordAssembly(Assembly.GetEntryAssembly());
+            container.Prepare();
+        }
+
+        static void TestDataProviderManager()
+        {
+            DataProviderManager manager = new DataProviderManager(container.Return<DataProvider>());
+            Console.WriteLine(manager.IsDataAvailable() == true);
+            Console.ReadLine();
+        }
+    }
+}
